3f610ba519c472f35ce8c42788740e977b384e75
Mike <mikecsomma@gmail.com>
2023-02-26 12:30:06 -0300

    De nuevo hago el vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 export default defineConfig( {
-    base: '/app-sumar/',
+    base: '/app-sumar/', // Para que funcione en github pages. /js-vite-blackjack/ es el nombre del repo.
     build: {
         outDir: './docs' // Se modifica de ./dist a ./docs para que funcione en github pages. 
     }
